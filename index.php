7a50a0f44719887013ea52bb52b0683074f4efbe
JeffNys <contact@fafache.net>
2021-08-25 20:31:43 +0200

    first commit

---
+++ index.php
@@ -0,0 +1,23 @@
+<!doctype html>
+<html lang="fr">
+
+<head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="/css/bootstrap.min.css">
+
+    <title>Jeff Nys</title>
+</head>
+
+<body>
+    <?php include("./partial/_navBar.php"); ?>
+    <div class="container">
+        <h1>Le site de Jeff Nys</h1>
+    </div>
+    <script src="/js/bootstrap.bundle.min.js"></script>
+</body>
+
+</html>
---
+++ notes.php
@@ -0,0 +1,23 @@
+<!doctype html>
+<html lang="fr">
+
+<head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="/css/bootstrap.min.css">
+
+    <title>Note</title>
+</head>
+
+<body>
+    <?php include("./partial/_navBar.php"); ?>
+    <div class="container">
+        <h1>Page de prise de note</h1>
+    </div>
+    <script src="/js/bootstrap.bundle.min.js"></script>
+</body>
+
+</html>
