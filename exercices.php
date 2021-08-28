db00bc7ae33d7f8edbf4b6c3f47700b4374f82d5
JeffNys <contact@fafache.net>
2021-08-28 21:31:37 +0200

    création de la page exo3.php

--- exercices.php
+++ exercices.php
@@ -20,6 +20,7 @@
         <ol>
             <li>premier exercice: si vous voyez ce site, c'est que c'est fait</li>
             <li><a href="/exo2.php">exercice 2</a>: décoder des chaines de caractère</li>
+            <li><a href="/exo3.php">exercice 3</a>: travailler avec des tableaux</li>
         </ol>
     </div>
     <script src="/js/bootstrap.bundle.min.js"></script>
