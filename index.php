f4f5f72683956bc355ba12e0e048d2ac3f7d4d92
JeffNys <contact@fafache.net>
2021-09-02 21:33:53 +0200

    add students websites

--- index.php
+++ index.php
@@ -19,8 +19,19 @@
 
         <h3>les liens des différents sites</h3>
         <ul>
-            <li><a href="https://arthurweb.yj.lu/public/">Arthur G.</a></li>
-            <li><a href="https://php.bdelanls.fr/">Bertrand D.</a></li>
+            <li><a href="https://arthurweb.yj.lu/public/" target="_blank">Arthur G.</a></li>
+            <li><a href="https://php.bdelanls.fr/" target="_blank">Bertrand D.</a></li>
+            <li><a href="https://roger.jeffnys.fr" target="_blank">Roger</a></li>
+            <li><a href="https://stephane.jeffnys.fr" target="_blank">stéphane</a></li>
+            <li><a href="http://nkjsyux.cluster029.hosting.ovh.net/" target="_blank">Romain C</a></li>
+            <li><a href="http://knadhxw.cluster029.hosting.ovh.net/" target="_blank">David C</a></li>
+            <li><a href="https://magdarline.jeffnys.fr" target="_blank">magdarline</a></li>
+            <li><a href="https://khalil.jeffnys.fr" target="_blank">khalil</a></li>
+            <li>Léo</li>
+            <li><a href="https://nadia.jeffnys.fr" target="_blank">nadia</a></li>
+            <li><a href="https://fatou.jeffnys.fr" target="_blank">Fatou</a></li>
+            <li><a href="https://johnd.ovh" target="_blank">Jonathan</a></li>
+            <li><a href="http://www.arthurtavernini.fr/" target="_blank">Arthur T</a></li>
         </ul>
 
     </div>
